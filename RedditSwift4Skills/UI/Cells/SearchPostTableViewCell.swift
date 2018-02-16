b9d2b8fbbbda933af2c5a6d0d77011dbb4ab815f
reich452 <reich452@regis.edu>
2018-02-15 20:09:26 -0700

    Set subclasses in storyboard and added outlets

---
+++ RedditSwift4Skills/UI/Cells/SearchPostTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SearchPostTableViewCell.swift
+//  RedditSwift4Skills
+//
+//  Created by Nick Reichard on 2/15/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class SearchPostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var upVoteLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var authorLabel: UILabel!
+    
+
+}
